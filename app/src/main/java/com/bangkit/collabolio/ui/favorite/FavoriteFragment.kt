09a3ee6d5577527ee4d610b763eea66442c9e310
Tiofd <103199257+Tiofd@users.noreply.github.com>
2023-06-16 19:38:53 +0700

    favorite coming soon

--- app/src/main/java/com/bangkit/collabolio/ui/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/bangkit/collabolio/ui/favorite/FavoriteFragment.kt
@@ -1,43 +1,17 @@
 package com.bangkit.collabolio.ui.favorite
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-<<<<<<< Updated upstream
-
-import android.widget.ArrayAdapter
-=======
 import android.widget.TextView
->>>>>>> Stashed changes
 import androidx.fragment.app.Fragment
-import com.bangkit.collabolio.R
-import com.bangkit.collabolio.adapters.CardsAdapter
-import com.bangkit.collabolio.adapters.FavoriteAdapter
+import androidx.lifecycle.ViewModelProvider
 import com.bangkit.collabolio.databinding.FragmentFavoriteBinding
-import com.bangkit.collabolio.utilities.Favorite
-import com.bangkit.collabolio.utilities.UserSwipe
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
 
 class FavoriteFragment : Fragment() {
 
     private var _binding: FragmentFavoriteBinding? = null
-    // get current user
-    val firebaseAuth = FirebaseAuth.getInstance()
-    val currentUser = firebaseAuth.currentUser
-
-    // initialize firestore database and fetch user's data that currently login in this app
-    @SuppressLint("StaticFieldLeak")
-    val db = FirebaseFirestore.getInstance()
-
-    // variables to fill the card layout using adapter
-    private var favoriteAdapter: ArrayAdapter<Favorite>? = null
-    private var rowItems = ArrayList<Favorite>()
-    private var users = mutableListOf<Favorite>()
-
 
     private val binding get() = _binding!!
 
@@ -46,9 +20,9 @@ class FavoriteFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        val favoriteViewModel =
+            ViewModelProvider(this)[FavoriteViewModel::class.java]
 
-    /*val favoriteViewModel =
-            ViewModelProvider(this)[FavoriteViewModel::class.java] */
         _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
@@ -62,49 +36,5 @@ class FavoriteFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        // Get a reference to the RecyclerView
-        val recyclerView = binding.rvFavorite
-        val userDocumentRef = db.collection("users").document(currentUser!!.uid)
-        userDocumentRef.get().addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                val userDocument = task.result
-                val userName = userDocument.getString("username")
-                val userEmail = userDocument.getString("email")
-                val userProfileURL = userDocument.getString("profile.photoURL")
-
-                fillingTheItems()
-
-            } else {
-                Log.d("PENG", "maaf ada error : ${task.exception}")
-            }
-        }
-        // Create a new instance of the FavoriteAdapter class and pass it the users list
-        val favoriteAdapter = FavoriteAdapter(requireContext(), users)
-        Log.d("DATAA", users.toString())
-        // Set the adapter of the RecyclerView to the FavoriteAdapter instance.
-        recyclerView.adapter = favoriteAdapter
-    }
-
-    private fun fillingTheItems() {
-        val cardsQuery = db.collection("users").whereEqualTo("profile.isMale", true).limit(5)
-        cardsQuery.get()
-            .addOnSuccessListener { documents ->
-                for (document in documents) {
-                    val user = document.toObject(Favorite::class.java)
-                    Log.d("SUKL", user.toString())
-                    if (!users.contains(user)) {
-                        users.add(user)
-                        favoriteAdapter?.notifyDataSetChanged()
-                    }
-                }
-            }
-            .addOnFailureListener { exception ->
-                Log.d("KARTU", "get failed with : ", exception)
-            }
     }
 }
--- app/src/main/java/com/bangkit/collabolio/ui/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/bangkit/collabolio/ui/favorite/FavoriteViewModel.kt
@@ -8,46 +8,8 @@ import androidx.lifecycle.ViewModel
 
 
 class FavoriteViewModel : ViewModel() {
-<<<<<<< Updated upstream
-    private val _userFavoriteData = MutableLiveData<List<UserProfileResponse?>>()
-    val userFavoriteData : LiveData<List<UserProfileResponse?>> = _userFavoriteData
-    fun getUserFavorite() {
-        val userId = FirebaseAuth.getInstance().currentUser?.uid
-        val db = FirebaseFirestore.getInstance()
-
-        if (userId != null) {
-            db.collection("users")
-                .document(userId)
-                .get()
-                .addOnSuccessListener { documentSnapshot ->
-                    if (documentSnapshot != null && documentSnapshot.exists()) {
-                        val userConnections = documentSnapshot.toObject(UserProfileResponse::class.java)?.connections
-                        if (!userConnections.isNullOrEmpty()) {
-                            val connectionUIDs = userConnections.map { it.uid }
-
-                            db.collection("users").whereIn("uid", connectionUIDs)
-                                .get()
-                                .addOnSuccessListener { querySnapshot ->
-                                    if (querySnapshot != null) {
-                                        val userProfile = documentSnapshot.toObject(
-                                            UserProfileResponse::class.java)
-                                        _userFavoriteData.value = listOf(userProfile)
-                                    }
-                                }
-                                .addOnFailureListener { exception ->
-                                    Log.d("UserViewModel", "error : $exception")
-                                }
-                        }
-                    }
-                }
-                .addOnFailureListener { exception ->
-                    Log.d("UserViewModel", "error : $exception")
-                }
-        }
-=======
     private val _text = MutableLiveData<String>().apply {
         value = "Coming Soon"
->>>>>>> Stashed changes
     }
     val text: LiveData<String> = _text
 }
